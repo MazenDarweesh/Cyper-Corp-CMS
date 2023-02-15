1bd1c5266d5d64d559f0b9913b1b63b426f8f1b1
MazenDarweesh <123546524+MazenDarweesh@users.noreply.github.com>
2023-02-15 10:29:40 +0200

    Update db_connection.php

--- includes/db_connection.php
+++ includes/db_connection.php
@@ -1,7 +1,7 @@
 <?php
 define("DB_SERVER" ,"localhost");
-define("DB_USER" ,"root");
-define("DB_PASS" ,"313619313619");
+define("DB_USER" ,"");
+define("DB_PASS" ,"");
 define("DB_NAME" ,"cyper_corp");
 
 $connection=mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
